85c568ce3cbff64f9e0ae098e0d4e23e55c9ae99
sigmak <dwfree74@gmail.com>
2021-06-01 16:26:09 +0900

    Ver 0.2
    야간수당 버튼 누르면 새창 나타나고
    거기서 계산한 값을
    기존 윈도우 창에 가져오는 거 까지 구현함.

--- wage.py
+++ wage.py
@@ -1,129 +1,158 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 31 14:10:46 2021
+Created on Mon May 31 16:48:42 2021
 
-@author: raven90
+@author: RYZEN3200G
 """
+# 참고1 : https://stackoverflow.com/questions/32259518/efficient-way-to-create-a-child-window-with-access-to-main-window-disabled/32259971
 from tkinter import *
 
 
-def set_test():
-    pass
- 
-# def set_test():
-#     win2 = Toplevel(root)
-#     win2.geometry('200x150')
-#     labelA = Label(win2, text = win2.e1.get())
-  
-# def createNewWindow(root):
-#     newWindow = Tk()
-#     newWindow.wm_attributes('-topmost',1) #최상위 창 설정
-#     labelA1 = Label(newWindow, text='최저시급')
-#     labelA1.grid(sticky=W, row=0, column=0) # sticky=W 왼쪽
+class MainWindow(Tk):
+
+    def __init__(self):
+        Tk.__init__(self)
+        self.title('알바비 산출 프로그램')
+        self.geometry('640x480+300+100') # 가로 * 세로 + X좌표 + Y좌표
+        self.resizable(False, False) # x(너비), y(높이) 값 변경 불가(창 크기 변경 불가)        
+        
+        label1 = Label(self, text='최저시급')
+        label1.grid(sticky=W, row=0, column=0) # sticky=W 왼쪽
+        
+        label2 = Label(self, text='근무날짜')
+        label2.grid(sticky=W, row=1, column=0)
+        
+        
+        label3 = Label(self, text='근무시간')
+        label3.grid(sticky=W, row=2, column=0)
+        
+        def CreateChildWindow(self):            
+            subWindow = Toplevel(self)
+            subWindow.title('야간수당 계산')
+            subWindow.geometry('300x150+500+200')
+            subWindow.resizable(False, False) # x(너비), y(높이) 값 변경 불가(창 크기 변경 불가)        
     
-#     labelB1 = Label(newWindow, text=root.e.get())
-#     labelB1.grid(sticky=W, row=0, column=1) # sticky=W 왼쪽
-
+            def quit_subwindow():
+                setlabel14(str(entryB5.get()))
+                subWindow.destroy()
+                self.grab_set()
     
-root = Tk()
-root.title('알바비 산출 프로그램')
-root.geometry('640x480+300+100') # 가로 * 세로 + X좌표 + Y좌표
-root.resizable(False, False) # x(너비), y(높이) 값 변경 불가(창 크기 변경 불가)
+            #Button(subWindow, text="whoami", command=lambda : print("A Toplevel child window.")).pack()
+            #Button(subWindow, text='Exit', command=quit_subwindow).pack()
+            
+            subWindow.grab_set()
+            
+            labelA1 = Label(subWindow, text='최저시급')
+            labelA1.grid(sticky=W, row=0, column=0) # sticky=W 왼쪽
+            labelA2 = Label(subWindow, text='야간근로시간')
+            labelA2.grid(sticky=W, row=1, column=0) # sticky=W 왼쪽
+            labelA3 = Label(subWindow, text='가산')
+            labelA3.grid(sticky=W, row=2, column=0) # sticky=W 왼쪽
 
-#popup =Toplevel(root)
-# root위에 다른 창을 띄우고 싶을 때는 Tk()를 한번 더 쓰면 안되고 Toplevel()을 쓴다.
-#출처: https://man-about-town.tistory.com/30 [YongWook's Notes]
+            labelA4 = Label(subWindow, text='---------------')
+            labelA4.grid(sticky=W, row=3, column=0) # sticky=W 왼쪽
+            
+            labelA5 = Label(subWindow, text='야간수당 합계')
+            labelA5.grid(sticky=W, row=4, column=0) # sticky=W 왼쪽
+            
+            labelB1 = Label(subWindow, text=getE1())
+            labelB1.grid(sticky=E, row=0, column=1) # sticky=E 오른쪽
+            entryB2 = Entry(subWindow, width=20, justify='right') # 한줄에 입력값 받을때 사용
+            entryB2.grid(row=1, column=1)
 
-label1 = Label(root, text='최저시급')
-label1.grid(sticky=W, row=0, column=0) # sticky=W 왼쪽
+            entryB3 = Entry(subWindow, width=20, justify='right') # 한줄에 입력값 받을때 사용
+            entryB3.grid(row=2, column=1)
+            tmpNum = '0.50'
+            entryB3.insert(0,tmpNum)
+            
+            labelB4 = Label(subWindow, text='-----------------------------')
+            labelB4.grid(sticky=W, row=3, column=1) # sticky=W 왼쪽
+            
+            entryB5 = Entry(subWindow, width=20, justify='right') # 한줄에 입력값 받을때 사용
+            entryB5.grid(row=4, column=1)
 
-label2 = Label(root, text='근무날짜')
-label2.grid(sticky=W, row=1, column=0)
-
-
-label3 = Label(root, text='근무시간')
-label3.grid(sticky=W, row=2, column=0)
-
-
-button4 = Button(root, text="야간수당", command=set_test)
-button4.grid(sticky=W, row=3, column=0)
-
-
-button5 = Button(root, text="추가수당")
-button5.grid(sticky=W, row=4, column=0)
-
-button6 = Button(root, text="세금")
-button6.grid(sticky=W, row=5, column=0)
-
-global e1
-e1 = Entry(root, width=20) # 한줄에 입력값 받을때 사용
-e1.grid(row=0, column=1)
-
-e2 = Entry(root, width=20) # 한줄에 입력값 받을때 사용
-e2.grid(row=1, column=1)
-
-e3 = Entry(root, width=20) # 한줄에 입력값 받을때 사용
-e3.grid(row=2, column=1)
-
-label14 = Label(root, text='0') # 한줄에 입력값 받을때 사용
-label14.grid(sticky=E,row=3, column=1)
-
-label15 = Label(root, text='0') # 한줄에 입력값 받을때 사용
-label15.grid(sticky=E,row=4, column=1)
-
-label16 = Label(root, text='0') # 한줄에 입력값 받을때 사용
-label16.grid(sticky=E,row=5, column=1)
+            def subIni():
+                entryB2.delete(0,END) # 라인 전체 삭제
+                entryB5.delete(0,END) # 라인 전체 삭제
+            def totsum():
+                tot = float(labelB1.cget("text")) * float(entryB2.get()) * float(entryB3.get())
+                entryB5.delete(0,END) # 라인 전체 삭제
+                entryB5.insert(0,str(tot))
+            
+            Button(subWindow, text="초기화" ,command=subIni ).grid(row=5, column=0)       # 초기화 버튼
+            Button(subWindow, text="합계", command=totsum).grid(row=5, column=1)          # 합계 버튼
+            Button(subWindow, text="Close", command=quit_subwindow).grid(row=5, column=2) # 종료 버튼
+            
 
 
-label21 = Label(root, text='원')
-label21.grid(sticky=W, row=0, column=2) # sticky=W 왼쪽
+        def click4():
+            popupwin = CreateChildWindow(self)
+            return popupwin # 해결방법 : https://stackoverflow.com/questions/47549304/attributeerror-nonetype-object-has-no-attribute-mainloop
+        
+        def getE1():
+            return e1.get()
+        def setE1(Num):
+            e1.insert(0, Num)
+            
+        def getE2():
+            return e2.get()
+        def setE2(Num):
+            e2.insert(0, Num)
+            
+        def getE3():
+            return e3.get()
+        def setE3(Num):
+            e3.insert(0, Num)
+            
 
-label22 = Label(root, text='일')
-label22.grid(sticky=W, row=1, column=2) # sticky=W 왼쪽
+        def getlabel14():
+            return label14.cget("text")
+        
+        def setlabel14(Num):
+            label14['text'] = Num
+            
 
-label23 = Label(root, text='시간')
-label23.grid(sticky=W, row=2, column=2) # sticky=W 왼쪽
+        
+        button4 = Button(self, text="야간수당", command=click4)
+        button4.grid(sticky=W, row=3, column=0)
+        
+        
+        button5 = Button(self, text="추가수당")
+        button5.grid(sticky=W, row=4, column=0)
+        
+        button6 = Button(self, text="세금")
+        button6.grid(sticky=W, row=5, column=0)
+        
+        e1 = Entry(self, width=20, justify='right') # 한줄에 입력값 받을때 사용
+        e1.grid(row=0, column=1)
+        tmpNum = '8720'
+        e1.insert(0,tmpNum)
+        
+        e2 = Entry(self, width=20) # 한줄에 입력값 받을때 사용
+        e2.grid(row=1, column=1)
+        
+        e3 = Entry(self, width=20) # 한줄에 입력값 받을때 사용
+        e3.grid(row=2, column=1)
+        
+        label14 = Label(self, text='0') # 한줄에 입력값 받을때 사용
+        label14.grid(sticky=E,row=3, column=1)
+        
+        label15 = Label(self, text='0') # 한줄에 입력값 받을때 사용
+        label15.grid(sticky=E,row=4, column=1)
+        
+        label16 = Label(self, text='0') # 한줄에 입력값 받을때 사용
+        label16.grid(sticky=E,row=5, column=1)
+        
+        
+        label21 = Label(self, text='원 (2021기준)')
+        label21.grid(sticky=W, row=0, column=2) # sticky=W 왼쪽
+        
+        label22 = Label(self, text='일')
+        label22.grid(sticky=W, row=1, column=2) # sticky=W 왼쪽
+        
+        label23 = Label(self, text='시간')
+        label23.grid(sticky=W, row=2, column=2) # sticky=W 왼쪽
 
-# def change():
-#     label1.config(text='또 만나요')
-    
-# btn = Button(root, text='클릭', command=change)
-# btn.pack()
-
-
-# e = Entry(root, width=30) # 한줄에 입력값 받을때 사용
-# e.pack()
-
-# def btncmd():
-#     print(txt.get('1.0',END))
-#     print(e.get())
-#     txt.delete('1.0',END))
-#     e.delete(0,END))
-
-
-# btn1 = Button(root, text='버튼1')
-# btn1.pack() # 이게 있어야 보임.
-
-# btn2 = Button(root, padx=5, pady=10, text='버튼2') # 여백
-# btn2.pack() # 이게 있어야 보임.
-
-# btn3 = Button(root, padx=10, pady=5, text='버튼3') # 여백
-# btn3.pack() # 이게 있어야 보임.
-
-# btn4 = Button(root, width=10, height=3, text='버튼4') #고정된 크기
-# btn4.pack() # 이게 있어야 보임.
-
-# btn5 = Button(root, fg='red', bg='yellow', text='버튼5') #고정된 크기
-# btn5.pack() # 이게 있어야 보임.
-
-# def btncmd():
-#     print('버튼이 클릭되었어요.')
-
-# btn7 = Button(root, text='동작하는 버튼', command=btncmd)
-# btn7.pack()
-
-
-root.mainloop()
-
-
-
+if __name__ == "__main__":
+    win = MainWindow()
+    win.mainloop()
