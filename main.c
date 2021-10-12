29bac8c6b03eab9a1023fc70bf1cecdbbaa18ad0
sigmak <dwfree74@gmail.com>
2021-10-12 17:47:02 +0900

    Ver-0.1
    2단부터 9단까지 가로 형태의 구구단 표를 출력하시오.

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+2단부터 9단까지 가로 형태의 구구단 표를 출력하시오.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i, j;
+
+	for (j = 1; j <= 9; j++) {
+		for (i = 2;i <= 9;i++) {
+			printf("%2dx%2d=%2d\t", i,j,i*j);
+		}
+		printf("\n");
+	}
+
+	system("pause");
+	return 0;
+}
+
