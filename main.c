602e4df0cc7644c36263f6118e2f70907102625b
sigmak <dwfree74@gmail.com>
2021-11-08 17:40:25 +0900

    Ver-0.1
    C언어 포인터 문제 빈칸 채우기
    
    [참고]
    *prt++;  // ptr이 가리키는 위치(포인터)를 1만큼 증가시킨다.
    //https://m.blog.naver.com/star7sss/220766291067
    
    
    int (*p)(int,int); //포인터형식으로 함수 선언
    //https://study-program.tistory.com/89

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+int add(int a, int b);
+int mult(int a, int b);
+int subt(int a, int b);
+
+int main(void) {
+	int(*pfunary[3])(int, int) = { 0, };
+	pfunary[0] = add;
+	pfunary[1] = mult;
+	pfunary[2] = subt;
+	char *ops = "*+-";
+	char op;
+	while (op = *ops++)
+	{
+		switch (op) {
+		case '+': printf("%c 결과: %d\n", op, pfunary[0](3, 5));
+			break;
+		case '-': printf("%c 결과: %d\n", op, pfunary[2](3, 5));
+			break;
+		case '*': printf("%c 결과: %d\n", op, pfunary[1](3, 5));
+			break;
+		}
+	}
+
+	system("pause");
+	return 0;
+}
+int add(int a, int b) {
+	return a + b;
+}
+int mult(int a, int b) {
+	return a * b;
+}
+int subt(int a, int b) {
+	return a - b;
+}
